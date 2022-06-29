8f39650d14f73affc3fa58399ebcf835033798b6
Vít Dolínek <vit.dolinek@smartlook.com>
2022-06-29 19:39:39 +0200

    chore: move type file

--- src/components/Icons/Units/UnitsIcon.tsx
+++ src/components/Icons/Units/UnitsIcon.tsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 import React from "react";
 import { FC } from "react";
-import { Units } from "./types";
+import { Units } from "../../../types/units";
 
 export interface IUnitsIconProps {
   size: 25 | 30 | 45 | 50 | 60 | 75 | 90;
@@ -12,7 +12,7 @@ const StyledUnitsIcon = styled("div")<IUnitsIconProps>`
   width: ${({ size }) => size}px;
   height: ${({ size }) => size}px;
   background-size: contain;
-  background-image: url(${({ size, unit }) => {
+  background-image: url(${({ unit }) => {
     try {
       return require(`../../../assets/images/units/${Units[unit]}.png`);
     } catch (e) {
